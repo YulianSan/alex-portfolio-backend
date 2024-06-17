818364ff260d6abe216e96e6130524f661e6ac4f
YulianSan <knoob0733@gmail.com>
2024-06-16 22:09:27 -0300

    feat: basic usage jwt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::get, Router};
 use dotenvy::dotenv;
+use jsonwebtoken::{encode, EncodingKey, Header};
+mod auth;
+use crate::auth::structs::claims::Claims;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(generate_jwt));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7979").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn generate_jwt() -> Result<(StatusCode, String), (StatusCode, String)> {
+    Ok((
+        StatusCode::OK,
+        encode(
+            &Header::default(),
+            &Claims {
+                user_id: String::from("1"),
+                exp: 3600,
+            },
+            &EncodingKey::from_secret("secret".as_ref()),
+        )
+        .unwrap(),
+    ))
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,16 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    refresh_tokens (id) {
+        id -> Int4,
+        #[max_length = 255]
+        token -> Varchar,
+        expire_at -> Timestamp,
+        user_id -> Uuid,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Uuid,
         #[max_length = 255]
@@ -13,3 +23,10 @@ diesel::table! {
         updated_at -> Timestamp,
     }
 }
+
+diesel::joinable!(refresh_tokens -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    refresh_tokens,
+    users,
+);
