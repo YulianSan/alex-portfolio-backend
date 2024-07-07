72f9be60e769d8a09baf568e0112a3fa9905d24f
YulianSan <knoob0733@gmail.com>
2024-07-07 20:22:53 -0300

    feat: adding basics imports

--- src/adapters/repositories/user_repository.rs
+++ src/adapters/repositories/user_repository.rs
@@ -1,21 +1,34 @@
 use async_trait::async_trait;
 use chrono::NaiveDateTime;
+use diesel::{
+    prelude::*,
+    r2d2::{ConnectionManager, Pool},
+};
 use uuid::Uuid;
 
 use crate::domain::{entities::user::User, repositories::user_repository};
+use crate::schema::users::dsl::*;
 
-pub struct UserRepository {}
+pub struct UserRepository {
+    pub db_connection: Pool<ConnectionManager<PgConnection>>,
+}
 
 #[async_trait]
 impl user_repository::UserRepository for UserRepository {
-    async fn create(&self) -> Result<crate::domain::entities::user::User, Box<dyn std::error::Error>> {
+    async fn create(
+        &self,
+    ) -> Result<crate::domain::entities::user::User, Box<dyn std::error::Error>> {
+        let mut connection = self.db_connection.get()?;
+
         Ok(User {
             id: Uuid::parse_str("123e4567-e89b-12d3-a456-426614174000").unwrap(),
             name: String::from("Yulian"),
             email: String::from("yulian@gmail.com"),
             password: String::from("132"),
-            updated_at: NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S").unwrap(),
-            created_at: NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S").unwrap(),
+            updated_at: NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")
+                .unwrap(),
+            created_at: NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")
+                .unwrap(),
         })
     }
 }
--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,16 +1,18 @@
 use std::sync::Arc;
 
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{http::StatusCode, Router};
 use tokio::net::TcpListener;
 
 use crate::adapters::{controllers, repositories::{token_repository::TokenRepository, user_repository::UserRepository}, shared::app_state::AppState};
 mod database;
 
 pub async fn server(listener: TcpListener, db_name: &str) -> Result<(), std::io::Error> {
+    let pool = database::get_connection_pool();
+
     let data = AppState {
         token_repository: Arc::new(TokenRepository {}),
-        user_repository: Arc::new(UserRepository {}),
-        db_pool: database::get_connection_pool()
+        user_repository: Arc::new(UserRepository { db_connection: pool.clone() }),
+        db_pool: pool
     }; 
 
     let app = Router::new().nest("/", controllers::routes()).with_state(data);
